ca0de417a80156605df367e069d398e85468a973
Tim H <tthu@sfu.ca>
2020-07-08 12:46:23 -0700

    tthu

---
+++ public/tim-script.js
@@ -0,0 +1,86 @@
+var oDoc, sDefTxt;
+document.getElementById("clearbutton").onclick = ClearField;
+document.getElementById('savebutton').onclick = SaveTxt;
+
+function initDoc() {
+  oDoc = document.getElementById("textBox");
+  sDefTxt = oDoc.innerHTML;
+  if (document.compForm.switchMode.checked) { setDocMode(true); }
+}
+
+function formatDoc(sCmd, sValue) {
+  if (validateMode()) { document.execCommand(sCmd, false, sValue); oDoc.focus(); }
+}
+
+function validateMode() {
+  if (!document.compForm.switchMode.checked) { return true ; }
+  alert("Uncheck \"Show HTML\".");
+  oDoc.focus();
+  return false;
+}
+
+function setDocMode(bToSource) {
+  var oContent;
+  if (bToSource) {
+    oContent = document.createTextNode(oDoc.innerHTML);
+    oDoc.innerHTML = "";
+    var oPre = document.createElement("pre");
+    oDoc.contentEditable = false;
+    oPre.id = "sourceText";
+    oPre.contentEditable = true;
+    oPre.appendChild(oContent);
+    oDoc.appendChild(oPre);
+    document.execCommand("defaultParagraphSeparator", false, "div");
+  } else {
+    if (document.all) {
+      oDoc.innerHTML = oDoc.innerText;
+    } else {
+      oContent = document.createRange();
+      oContent.selectNodeContents(oDoc.firstChild);
+      oDoc.innerHTML = oContent.toString();
+    }
+    oDoc.contentEditable = true;
+  }
+  oDoc.focus();
+}
+
+function ClearField(){
+  document.getElementById("textBox").value = "";
+}
+
+function printDoc() {
+  if (!validateMode()) { return; }
+  var oPrntWin = window.open("","_blank","width=450,height=470,left=400,top=100,menubar=yes,toolbar=no,location=no,scrollbars=yes");
+  oPrntWin.document.open();
+  oPrntWin.document.write("<!doctype html><html><head><title>Print<\/title><\/head><body onload=\"print();\">" + oDoc.innerHTML + "<\/body><\/html>");
+  oPrntWin.document.close();
+}
+
+
+//  Credit: see source.txt
+function SaveTxt() {
+      var userinput = document.getElementById('textBox').value;
+      var blob = new Blob([ userinput ], { type: 'text/plain' });
+      var filename = "text.txt";
+
+      var link = document.createElement("a");
+      link.download = filename;
+      link.innerHTML = "Download File";
+      if (window.webkitURL != null) {
+        // Chrome allows the link to be clicked without actually adding it to the DOM.
+        link.href = window.webkitURL.createObjectURL(blob);
+      } else {
+        // Firefox requires the link to be added to the DOM before it can be clicked.
+        link.href = window.URL.createObjectURL(blob);
+        link.onclick = removeLink;
+        link.style.display = "none";
+        document.body.appendChild(link);
+      }
+
+      link.click();
+    }
+
+function removeLink(event) {
+  // remove the link from the DOM
+  document.body.removeChild(event.target);
+}
