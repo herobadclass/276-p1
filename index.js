aaecb46f048b815def16f941c291e9229db7dad2
Tim H <tthu@sfu.ca>
2020-07-26 15:35:57 -0700

    s

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ app.get('/', checkAuthenticated, (req, res) => {
     if (error) {
       console.log(error);
     }
-    res.render('pages/index', {'list':JSON.stringify(result.rows)})
+    res.render('pages/index', {'list':JSON.stringify(result.rows), name: req.body.name })
   })
 })
 
