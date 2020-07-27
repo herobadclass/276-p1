c9c7dfc9297083dd950bf2b7f9a2b9cb6ce6c051
Tim H <tthu@sfu.ca>
2020-07-27 14:32:34 -0700

    1placeholder

--- index.js
+++ index.js
@@ -51,9 +51,13 @@ var io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  // socket.on('disconnect', () => {
+  //   console.log('user disconnected');
+  // });
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
+
 });
 
 // http.listen(3000, () => {
