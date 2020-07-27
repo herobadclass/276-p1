0eb5d68784c180b9cd2b4598985acc1e29cb4140
Tim H <tthu@sfu.ca>
2020-07-27 16:23:33 -0700

    1placeholder

--- index.js
+++ index.js
@@ -56,11 +56,12 @@ io.on('connection', (socket) => {
   });
 
 });
-
-socket.on('disconnect', () => {
+// socket.on('disconnect', () => {
+//     console.log('user disconnected');
+//   });
+io.on('disconnect'() => {
     console.log('user disconnected');
   });
-
 // http.listen(3000, () => {
 //   console.log('listening on *:3000');
 // });
