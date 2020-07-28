62c11d980e5a3c507dcc9c3a3325d3b1459e66b9
Tim H <tthu@sfu.ca>
2020-07-27 23:55:40 -0700

    1placeholder

--- index.js
+++ index.js
@@ -65,6 +65,25 @@ io.on('connection', (socket) => {
   io.to(sessionID).emit('bleh', 'HI!!!!!');
 });
 
+var parseCookie = require('connect').utils.parseCookie;
+ 
+io.set('authorization', function (data, accept) {
+    // check if there's a cookie header
+    if (data.headers.cookie) {
+        // if there is, parse the cookie
+        data.cookie = parseCookie(data.headers.cookie);
+        // note that you will need to use the same key to grad the
+        // session id, as you specified in the Express setup.
+        data.sessionID = data.cookie['express.sid'];
+    } else {
+       // if there isn't, turn down the connection with a message
+       // and leave the function.
+       return accept('No cookie transmitted.', false);
+    }
+    // accept the incoming connection
+    accept(null, true);
+});
+
 
 // socket.on('disconnect', () => {
 //     console.log('user disconnected');
@@ -296,8 +315,7 @@ app.get('/user_count', (req, res) => {
   })
 })
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-var sio = io.listen(app);
+// app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-// http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 module.exports = app
