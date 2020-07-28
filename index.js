738e2efa3847580a4b702b3cf8d3669446398a0e
Tim H <tthu@sfu.ca>
2020-07-28 00:40:49 -0700

    1placeholder

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
   console.log("its session id:");
   sessionID = socket.id;
   console.log(sessionID);
-  socket.broadcast.emit('new user', sessionID);
+  io.emit('new user', sessionID);
 
   socket.on('chat message', (msg) => {
     io.emit('chat message', msg);
